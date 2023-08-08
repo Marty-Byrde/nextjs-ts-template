bad3048863b64728011d5c569c26f3d040207b8a
Marty-Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2023-08-08 09:32:07 +0200

    feat: added new Navbar to root-layout
    Also modified the footer text, depending on the screen-size. Also the entire page itself is now stretched using h-full, so that the defined background is present no matter what.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,37 +1,56 @@
 import '@/public/globals.css'
-import React, { CSSProperties } from "react"
-import { getColorMode, useColorModeValue } from "@/app/(components)/ColorModeHandler"
-import DaisyNavBar from "./DaisyNavBar"
-import ColorModeSwitcher from "@/app/(components)/ColorModeSwitcher";
+import React from "react"
+import { useColorModeValue } from "@/app/(components)/ColorModeHandler"
+import NavigationBar, { NavigationBarProps } from "@/app/(components)/Navigation/NavigationBar"
 
+const navigationBarProps: NavigationBarProps = {
+  title: "Dashboard",
+  items: [
+    {
+      label: 'First Category',
+      image: "https://cdn-icons-png.flaticon.com/512/74/74905.png",
+      children: [
+        {
+          label: 'SubItem',
+          subLabel: "Label of subitem",
+          href: '/route'
+        },
+        {
+          label: 'Second Subitem',
+          subLabel: 'Label of second subitem',
+          href: '/',
+        },
+      ],
+    }
+  ]
+}
 
 
 export default async function RootLayout({ children, }: { children: React.ReactNode }) {
-  const background = 'bg-base-100/90 dark:bg-base-100'
+  const background = 'bg-stone-100/90 dark:bg-base-100'
 
 
   return (
-    <html lang="en" data-theme={useColorModeValue("cmyk", "halloween")} className={`${background} ${useColorModeValue("", "dark")}`}>
+    <html lang="en" data-theme={useColorModeValue("cmyk", "halloween")} className={`h-full ${background} ${useColorModeValue("", "dark")}`}>
 
-      <body className={``}>
+    <body className='h-full bg-gradient-to-r from-base-100 to-base-100'>
 
-      <div className='flex justify-center my-4 items-center'>
-        <ColorModeSwitcher />
+    <NavigationBar {...navigationBarProps} />
+
+    <div className={`p-4 pb-6 mb-10 ${background}`}>
+      {children}
+    </div>
+
+
+    <footer className="footer footer-center p-4 bg-gray-400/50 dark:bg-base-300 text-base-content fixed bottom-0 right-0 left-0">
+      <div>
+        <p className='hidden md:block'>Copyright © 2023 - All right reserved by @Marty-Byrde</p>
+        <p className='block md:hidden'>Copyright © @Marty-Byrde</p>
       </div>
-        {/*<DaisyNavBar />*/}
-        <div className={`p-4 ${background} `} id={"childrenContainer"}>
-          {children}
-        </div>
+    </footer>
 
 
-        <footer className="hidden footer footer-center p-4 bg-gray-400/50 dark:bg-base-300 text-base-content fixed bottom-0 right-0 left-0">
-          <div>
-            <p>Copyright © 2023 - All right reserved by XY</p>
-          </div>
-        </footer>
-
-
-      </body>
+    </body>
     </html>
   )
 }
